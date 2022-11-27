333405b43612c45259530eef7b3401bdad8065a1
Terry Grimoire <terry.grimoire@gmail.com>
2022-11-27 19:47:29 +0400

    adding mentions legales

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,6 +4,8 @@ import facebook from "../../assets/facebook.svg";
 import youtube from "../../assets/youtube.svg";
 import linkedin from "../../assets/linkedin.svg";
 import tiktok from "../../assets/tiktok.png";
+import region from "../../assets/logo_reunion.png";
+import ue from "../../assets/logo_UE.png";
 import instagram from "../../assets/instagram.svg";
 
 function Footer() {
@@ -63,6 +65,22 @@ function Footer() {
             <a href="https://www.linkedin.com/in/yannick-peria-21253a1b0/">
               <img src={linkedin} alt="logo de Linkedin" />
             </a>
+          </li>
+        </ul>
+      </div>
+      <div className="mentions_obligatoires">
+        <h4>Co-Financement</h4>
+        <small>
+          Ce site a été financé avec l’aide du FEDER (REACT-UE), dans le cadre
+          de la réponse de l’Union européenne à la pandémie COVID-19. L’Europe
+          s’engage à La Réunion
+        </small>
+        <ul className="flex align-center justify-start">
+          <li>
+            <img src={region} alt="logo de Facebook" />
+          </li>
+          <li>
+            <img src={ue} alt="logo de Tiktok" className="tiktok" />
           </li>
         </ul>
       </div>
--- src/pages/Mentions.jsx
+++ src/pages/Mentions.jsx
@@ -6,15 +6,15 @@ function Mentions() {
   }, []);
 
   const information = {
-    siteLink: " https://lemontecito.fr ",
-    companyName: " Le Montecito ",
-    ownerName: " Simon Giraudet ",
-    email: " lemontecito1011@gmail.com ",
-    phone: " 09 86 50 12 07 ",
-    address: " 6 All. Duquesne, 44000, Nantes.",
-    hostName: " 1and1 ",
-    hostAddress: " Test des addresses 44300 Host",
-    hostPhone: " 01.02.03.04.05 ",
+    siteLink: " https://www.yannickperia.com ",
+    companyName: " Yannick Perianayagom-Mary",
+    ownerName: " Yannick Perianayagom-Mary ",
+    email: " yannickperia@gmail.com ",
+    phone: " 06 92 32 86 86",
+    address: " 	29 B RUE DE BORDEAUX 97420 LE PORT",
+    hostName: "Hostinger",
+    hostAddress: "143 Rue Emile Julien, 34070 Montpellier, France",
+    hostPhone: " 08 92 97 70 93 ",
   };
 
   return (
